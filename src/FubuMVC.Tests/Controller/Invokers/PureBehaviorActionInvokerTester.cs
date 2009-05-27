13b5c27832758e41eb34357d5ccd5e471e33d521
Chad Myers <chad.myers@gmail.com>
2009-05-27 01:34:49 +0000

    Added test to verify that the input model is getting populated by the PureBehaviorActionInvoker

--- src/FubuMVC.Tests/Controller/Invokers/PureBehaviorActionInvokerTester.cs
+++ src/FubuMVC.Tests/Controller/Invokers/PureBehaviorActionInvokerTester.cs
@@ -10,16 +10,33 @@ namespace FubuMVC.Tests.Controller.Invokers
     public class PureBehaviorActionInvokerTester
     {
         private IControllerActionBehavior _behavior;
-        private PureBehaviorActionInvoker<object, object> _invoker;
+        private PureBehaviorActionInvoker<TestInputModel, TestOutputModel> _invoker;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _behavior = MockRepository.GenerateStub<IControllerActionBehavior>();
+            _invoker = new PureBehaviorActionInvoker<TestInputModel, TestOutputModel>(_behavior);
+        }
 
         [Test]
         public void should_invoke_the_first_behavior()
         {
-            _behavior = MockRepository.GenerateStub<IControllerActionBehavior>();
-            _invoker = new PureBehaviorActionInvoker<object, object>(_behavior);
             _invoker.Invoke(new Dictionary<string, object>());
 
-            _behavior.AssertWasCalled(b => b.Invoke<object, object>(null, null), o => o.IgnoreArguments());
+            _behavior.AssertWasCalled(b => b.Invoke<TestInputModel, TestOutputModel>(null, null), o => o.IgnoreArguments());
+        }
+
+        [Test]
+        public void should_populate_the_input_model()
+        {
+            var dict = new Dictionary<string, object> {{"PropInt", 4}};
+
+            var catcher = _behavior.CaptureArgumentsFor(b => b.Invoke<TestInputModel, TestOutputModel>(null, null), o=>o.Return(new TestOutputModel()));
+            
+            _invoker.Invoke(dict);
+
+            catcher.First<TestInputModel>().PropInt.ShouldEqual(4);
         }
     }
 }
